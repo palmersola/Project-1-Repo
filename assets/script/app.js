0d0f2a1f74fd3a28e89bc20f9d3905d6ea44a83e
palmersola <palmersola@yahoo.com>
2022-07-11 15:04:20 -0500

    added people in space api to js

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,14 +1,17 @@
 const roverAPIKey = "Q7oEuNeavWgiwaPavAuvpRyjlBVQzReLygSbcI3W";
-let sol = 3495;
+let sol = 2000;
+let inSpaceQueryURL = "http://api.open-notify.org/astros.json";
 let roverQueryURL =
   "http://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" +
   sol +
   "&api_key=" +
   roverAPIKey;
-let inSpaceQueryURL = "http://api.open-notify.org/astros.json?callback=?";
-console.log(roverQueryURL);
 fetch(roverQueryURL).then(response => response.json()).then(data => {
-  let imgSrc = data.photos[10].img_src;
+  let imgSrc = data.photos[17].img_src;
   $("main").text(imgSrc);
   $("img").attr("src", imgSrc);
+});
+fetch(inSpaceQueryURL).then(response => response.json()).then(data => {
+  console.log(data.number);
+  $("aside").text("There are " + data.number + " people currently in space!");
 });
