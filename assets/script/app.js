4d59465d04937f5d2ba1ab9a675b4694b99a2a24
odinarussell@gmail.com <odinarussel@gmail.com>
2022-07-13 10:40:34 -0500

    issues pulling

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,35 +6,38 @@ let roverQueryURL =
   sol +
   "&api_key=" +
   roverAPIKey;
-$("#random").click(function(event) {
+$("#random").click(function (event) {
   event.preventDefault();
   setImg();
 });
 function setImg() {
-
-  fetch(roverQueryURL).then(response => response.json()).then(data => {
-    let i = Math.floor(Math.random() * data.photos.length);
-    let imgSrc = data.photos[i].img_src;
-    if (imgSrc == "undefined") {
-      setImg();
-    } else {
-      $("#rover-img").attr("src", imgSrc);
-      $("#rover-name").text("Rover: " + data.photos[i].rover.name);
-      $("#solar").text("Solar date since landed: " + data.photos[i].sol);
-      $("#earth").text("Terrestrial date: " + data.photos[i].earth_date);
-    }
-  });
+  fetch(roverQueryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      let i = Math.floor(Math.random() * data.photos.length);
+      let imgSrc = data.photos[i].img_src;
+      if (imgSrc == "undefined") {
+        setImg();
+      } else {
+        $("#rover-img").attr("src", imgSrc);
+        $("#rover-name").text("Rover: " + data.photos[i].rover.name);
+        $("#solar").text("Solar date since landed: " + data.photos[i].sol);
+        $("#earth").text("Terrestrial date: " + data.photos[i].earth_date);
+      }
+    });
 }
 setImg();
-fetch(inSpaceQueryURL).then(response => response.json()).then(data => {
-  console.log(data.number);
-  $("#space-number").text(data.number + " people are currently in space");
-  for (i = 0; i < data.number; i++) {
-    let craft = data.people[i].craft;
-    if (craft == "ISS") {
-      $("#iss").append("<li>" + data.people[i].name + "</li>");
-    } else if (craft == "Tiangong") {
-      $("#tss").append("<li>" + data.people[i].name + "</li>");
+fetch(inSpaceQueryURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.number);
+    $("#space-number").text(data.number + " people are currently in space");
+    for (i = 0; i < data.number; i++) {
+      let craft = data.people[i].craft;
+      if (craft == "ISS") {
+        $("#iss").append("<li>" + data.people[i].name + "</li>");
+      } else if (craft == "Tiangong") {
+        $("#tss").append("<li>" + data.people[i].name + "</li>");
+      }
     }
-  }
-});
+  });
